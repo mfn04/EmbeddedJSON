86512021052d250150443eca7c13110c3a9b514f
Muhanna AlNoaimi <mfn.45@outlook.com>
2025-07-19 20:24:14 +0300

    Added objects and a pretty print function

--- src/main.c
+++ src/main.c
@@ -2,39 +2,47 @@
 #include "stdint.h"
 #include "stdbool.h"
 #include "stdarg.h"
+#include "stdio.h"
+#include "freertos/FreeRTOS.h"
 
-#define OBJECT(...) ({ field_data arr[] = (field_data[]){__VA_ARGS__}; (json_object) {  \
-    .data_size = sizeof(arr)/sizeof(field_data), \
-    .data =  arr , \
+#define OBJECT(...) ({ json_field arr[] = {__VA_ARGS__}; (json_object) {  \
+    .field_size = sizeof(arr)/sizeof(json_field), \
+    .field =  arr , \
 }; })
 
-#define FIELD_NUMBER(k, v) (field_data) { \
+#define FIELD_NUMBER(k, v) (json_field) { \
     .key = k, \
-    .type = TYPE_NUMBER, \
+    .value_type = TYPE_NUMBER, \
     .value = { \
         .number = v \
     } \
 }
-#define FIELD_STRING(k, v) (field_data) { \
+#define FIELD_STRING(k, v) (json_field) { \
     .key = k, \
-    .type = TYPE_STRING, \
+    .value_type = TYPE_STRING, \
     .value = { \
         .string = v \
     } \
 }
-#define FIELD_OBJECT(k, v)
+#define FIELD_OBJECT(k, v) (json_field) { \
+    .key = k, \
+    .value_type = TYPE_OBJECT, \
+    .value = { \
+        .object = v \
+    } \
+}
 #define FIELD_ARRAY(k, v)
 
-#define FIELD_BOOLEAN(k, v) (field_data) { \
+#define FIELD_BOOLEAN(k, v) (json_field) { \
     .key = k, \
-    .type = TYPE_BOOLEAN, \
+    .value_type = TYPE_BOOLEAN, \
     .value = { \
         .boolean = v \
     } \
 }
-#define FIELD_NULL(k,v) (field_data) { \
+#define FIELD_NULL(k,v) (json_field) { \
     .key = k, \
-    .type = TYPE_NULL, \
+    .value_type = TYPE_NULL, \
     .value = { \
         .number = v \
     } \
@@ -44,27 +52,75 @@ typedef enum {
     TYPE_NUMBER,
     TYPE_ARRAY,
     TYPE_BOOLEAN,
+    TYPE_OBJECT,
     TYPE_STRING,
     TYPE_NULL
 } field_type;
 
-struct json_object;
+struct json_field;
 
 typedef struct {
+    int field_size;
+    struct json_field* field;
+} json_object;
+
+typedef struct json_field {
     const char* key;
-    field_type type;
+    field_type value_type;
     union value_t {
         uint32_t number;
         const char* string;
-        struct json_object* object;
+        json_object object;
         bool boolean;
     } value;
-} field_data;
+} json_field;
 
-typedef struct {
-    int data_size;
-    field_data* data;
-} json_object;
+void pretty_print(json_object* object, int start_tab){
+    int tab = start_tab;
+    printf("{\n");
+    tab++;
+    for(int i = 0; i < object->field_size; i++){
+        int tab_done = 0;
+        while(tab_done < tab){
+            printf("\t");
+            tab_done++;
+        }
+        json_field field = object->field[i];
+        printf("\"%s\":", field.key);
+        switch (field.value_type)
+        {
+        case TYPE_STRING:
+            printf("\"%s\"\n", field.value.string);
+            break;
+        
+        case TYPE_NUMBER:
+            printf("%ld\n", field.value.number);
+            break;
+        
+        case TYPE_BOOLEAN:
+            if(field.value.boolean){
+                printf("true\n");
+            } else {
+                printf("false\n");
+            }
+            break;
+        
+        case TYPE_OBJECT:
+            pretty_print(&field.value.object,tab);
+            break;
+        
+        case TYPE_NULL:
+            printf("null\n");
+            break;
+        default:
+            break;
+        }
+    }
+    for(int i = 0; i < start_tab; i++){
+        printf("\t");
+    }
+    printf("}\n");
+}
 
 void app_main() {
 
@@ -73,10 +129,16 @@ void app_main() {
     asd = (int[]){4,5,6,7};
 
 
-    field_data data = FIELD_NUMBER("Hello", 5);
+    json_field data = FIELD_NUMBER("Hello", 5);
 
     json_object object = OBJECT(
         FIELD_NUMBER("Balance", 50),
-        FIELD_STRING("First_Name", "Hello"));
+        FIELD_STRING("First_Name", "Hello"),
+        FIELD_OBJECT("OBJECT",OBJECT(FIELD_NUMBER("TestSub",50))));
+
+    while(1){
+        pretty_print(&object,0);
+        vTaskDelay(pdMS_TO_TICKS(5000));
+    }
 
 }
